bdd751186d1600bf63d86379708dc94dfe996faf
Łukasz Bigos <luki.bigos@gmail.com>
2022-11-23 20:14:40 +0100

    added bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
-  return <div>Todo App</div>;
+  return <div className="container">Todo App</div>;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.css";
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
